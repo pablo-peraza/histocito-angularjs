4e1c761a64a6dbba655540f7e185d549817ac5cc
ciris <fabio.flores@ciriscr.com>
2017-12-07 16:06:27 -0600

    feat: generar reportes de diagnoticos de citologias por doctor y rango de fechas

--- src/app/admin/reportes/index.js
+++ src/app/admin/reportes/index.js
@@ -4,5 +4,6 @@ var modulo = angular.module( "Proyecto.reportes", [] );
 
 modulo.config( require( "./rutas" ) );
 modulo.controller( "reporteCtrl", require( "./ctrls/reportesCtrl.js" ) );
+modulo.controller( "DiagReporteCtrl", require( "./ctrls/diagReporteCtrl.js" ) );
 
 module.exports = modulo.name;
--- src/app/admin/reportes/rutas.js
+++ src/app/admin/reportes/rutas.js
@@ -14,4 +14,12 @@ function rutas( $routeProvider ) {
     //TODO: HAY QUE CREAR PERMISOS PARA ESTE PROCEDIMIENTO
     permisos: [ permisos.valores.laboratorio, permisos.valores.digitador ]
   } );
+
+  $routeProvider.when( "/inicio/admin/citologias", {
+    templateUrl: "admin/reportes/htmls/diagnosticosCitologias.html",
+    controller: "DiagReporteCtrl",
+    controllerAs: "vm",
+    titulo: "Diagnósticos de Citologías",
+    permisos: [ permisos.valores.laboratorio, permisos.valores.medico ]
+  } );
 }
