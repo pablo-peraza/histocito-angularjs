d99edc26a201b1f78a4d4aba08e4457e3de27986
ciris <fabio.flores@ciriscr.com>
2018-05-22 15:43:40 -0600

    feat: crear pantalla para gestionar relaciones de objetos

---
+++ src/app/admin/relaciones/ctrls/relacionesCtrl.js
@@ -0,0 +1,94 @@
+"use strict";
+
+module.exports = RelacionesCtrl;
+
+var _ = require( "lodash" );
+
+RelacionesCtrl.$inject = [ "$scope", "relaciones", "Muestras", "Usuarios", "RelacionesAPI" ];
+function RelacionesCtrl( $scope, relaciones, Muestras, Usuarios, RelacionesAPI ) {
+  var vm = this;
+  vm.mostrarForm = false;
+  vm.relaciones = relaciones;
+  vm.nuevo = { autorizados: [] };
+  vm.autorizado = null;
+  vm.guardar = guardar;
+  vm.cancelar = cancelar;
+  vm.editar = editar;
+  vm.eliminar = eliminar;
+  vm.agregarAutorizado =  agregarAutorizado;
+  vm.borrarAutorizado = borrarAutorizado;
+  vm.buscarMedicos = buscarMedicos;
+  vm.buscarDuenos = buscarDuenos;
+  vm.buscarClinicas = buscarClinicas;
+  vm.buscarUsuarios = buscarUsuarios;
+
+  function guardar( rel ) {
+    return RelacionesAPI.guardar( rel ).then(function( resp ) {
+      vm.relaciones.push( resp );
+      vm.cancelar();
+    } );
+  }
+
+  function cancelar() {
+    vm.nuevo = {};
+    vm.mostrarForm = false;
+  }
+
+  function editar( item ) {
+    vm.nuevo = item;
+    vm.mostrarForm = true;
+  }
+
+  function eliminar( item ) {
+    if( confirm( "¿Está seguro que desea eliminar esta relación?" ) ) {
+      return RelacionesAPI.eliminar( item._id ).then(function() {
+        vm.relaciones = _.reject( vm.relaciones, function( rel ) {
+          return rel._id === item._id;
+        } );
+      } );
+    }
+    return false;
+  }
+
+  function ok( resp ) {
+    return _.map( resp.data.lista, function( item ) {
+      item._id = item.id;
+      return item;
+    } );
+  }
+
+  function error() {
+    return [];
+  }
+
+  function agregarAutorizado(item) {
+    item._id = item.id;
+    vm.nuevo.autorizados.push(item);
+    vm.autorizado = null;
+  }
+
+  function borrarAutorizado(item) {
+    vm.nuevo.autorizados = _.reject(vm.nuevo.autorizados, function( aut ) {
+      return aut._id === item._id;
+    } );
+  }
+
+  function buscarMedicos( valor ) {
+    return Muestras.buscarMedicos( 0, 10, valor ).then( ok, error );
+  }
+
+  function buscarDuenos( texto ) {
+    var dim = [ {
+      tipoUsuario: [ "médico" ]
+    } ];
+    return Usuarios.buscar( 0, 20, texto, dim ).then( ok, error );
+  };
+
+  function buscarClinicas( valor ) {
+    return Muestras.buscarClinicas( 0, 10, valor ).then( ok, error );
+  }
+
+  function buscarUsuarios( valor ) {
+    return Muestras.buscarUsuarios( 0, 10, valor ).then( ok, error );
+  };
+} //controller
---
+++ src/app/admin/relaciones/ctrls/rest/relacionesAPI.js
@@ -0,0 +1,50 @@
+"use strict";
+
+module.exports = RelacionesAPI;
+
+RelacionesAPI.$inject = [ "$http", "node" ];
+function RelacionesAPI( $http, node ) {
+  RelacionesAPI.listar = listar;
+  RelacionesAPI.guardar = guardar;
+  RelacionesAPI.eliminar = eliminar;
+  return RelacionesAPI;
+
+  function listar() {
+    return $http.get( node + "/api/relaciones" )
+      .then(function(resp) {
+        return resp.data;
+      })
+      .catch(function() {
+        return [];
+      });
+  }
+
+  function ok( resp ) {
+    return resp.data;
+  }
+
+  function error( resp ) {
+    return resp;
+  }
+
+  function guardar( obj ) {
+    if ( obj._id ) {
+      return editar( obj );
+    } else {
+      return crear( obj );
+    }
+  }
+
+  function crear( obj ) {
+    console.log(obj);
+    return $http.post( node + "/api/relaciones", obj ).then( ok, error );
+  }
+
+  function editar( obj ) {
+    return $http.put( node + "/api/relaciones/" + obj._id, obj ).then( ok, error );
+  }
+
+  function eliminar(id) {
+    return $http.delete( node + "/api/relaciones/" + id ).then( ok, error );
+  }
+}
